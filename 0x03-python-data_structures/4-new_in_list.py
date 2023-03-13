1703f7b00100cd7375fda6c799ef5a5c86cccd6e
Death-thekidd <ohiemidivine7@gmail.com>
2023-03-14 00:36:22 +0100

    [update]: added new scripts

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    cp_list = my_list[:]
-    if idx < 0:
-        return (cp_list)
+    length = len(my_list)
 
-    length = len(cp_list)
+    new_list = my_list[:]
 
-    if idx > length - 1:
-        return (cp_list)
-    cp_list[idx] = element
-    return (cp_list)
-    
+    if 0 <= idx < length:
+        new_list[idx] = element
+
+    return (new_list)
