f4d0eaf757f9790e3e20837ca60dec4787bcf489
Death-thekidd <ohiemidivine7@gmail.com>
2023-03-14 02:37:11 +0100

    [update]: added new scripts

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if not matrix:
-        print("")
-    for i in matrix:
-        for j in range(len(i)):
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
             if j != 0:
-                print(" ", end="")
-            print("{:d}".format(i[j]), end="")
-        print("")
+                print(" ", end='')
+            print("{:d}".format(matrix[i][j]), end='')
+        print()
